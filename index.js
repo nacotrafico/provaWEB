1a074933fa952cb2cb6881c0e030545d48223ba6
nacotrafico <164434874+nacotrafico@users.noreply.github.com>
2024-04-25 00:48:53 +0000

    prova

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 async function listaUF(){
     var lista = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(res => {return res.json()})
     for (var i = 0; i < lista.length; i++){
-      document.createElement('li')
-      document.ul.appendChild(li)
+      console.log(lista[i])
     }
 } 
 document.addEventListener('DOMContentLoaded', function () {
